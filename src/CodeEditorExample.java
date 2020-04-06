22e84bacbc50f4fc915c4c0830feb54a3b259fc1
Avinash <avinashdamu323@gmail.com>
2020-04-05 22:58:56 -0400

    Toolbar Implementation

--- src/CodeEditorExample.java
+++ src/CodeEditorExample.java
@@ -1,9 +1,19 @@
+package application;
+
+import java.io.File;
+import java.io.InputStream;
+
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ToolBar;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBoxBuilder;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.VBoxBuilder;
@@ -15,45 +25,104 @@ import javafx.stage.Stage;
  * a JavaFX WebView.
  */
 public class CodeEditorExample extends Application {
-  // some sample code to be edited.
-  static final private String editingCode = "";
+	// some sample code to be edited.
+	static final private String editingCode = "";
 
 
-public void CodeEditorExample(String[] args) {};
+	public void CodeEditorExample(String[] args) {};
 
-  @Override 
-  public void start(Stage stage) throws Exception {
-    // create the editing controls.
-    
-	  Label title = new Label("CodeEditor");
-      
-	  title.setStyle("-fx-font-size: 20;");
-    
-    final Label labeledCode = new Label(editingCode);
-    final CodeEditor editor = new CodeEditor(editingCode);
-    final Button revertEdits = new Button("Revert edits");
-    
-    revertEdits.setOnAction(new EventHandler<ActionEvent>() {
-    
-    	@Override 
-      public void handle(ActionEvent actionEvent) {
-        editor.revertEdits();
-      }
-    });
- 
+	@Override 
+	public void start(Stage stage) throws Exception {
+		// create the editing controls.
 
-    // layout the scene.
-    final VBox layout = VBoxBuilder.create().spacing(10).children(
-      title,
-      editor,
-      HBoxBuilder.create().spacing(10).children(revertEdits).build(),
-      labeledCode
-    ).build();
-    layout.setStyle("-fx-background-color: cornsilk; -fx-padding: 10;");
+		Label title = new Label("CodeEditor");
 
-    // display the scene.
-    final Scene scene = new Scene(layout);
-    stage.setScene(scene);
-    stage.show();
-  }
+		title.setStyle("-fx-font-size: 20;");
+		final Label labeledCode = new Label(editingCode);
+		final CodeEditor editor = new CodeEditor(editingCode);
+		final Button revertEdits = new Button("Revert edits");
+		
+		//Toolbar Buttons
+		ToolBar toolBar = new ToolBar();
+		
+		File imageFile = new File("C:/Users/Avinash/workspace_hci/hci/src/images/hammer.png");  
+		Image imageDecline = new Image(imageFile.toURI().toString(),20,20,true,true);
+		Button compile = new Button("Compile",new ImageView(imageDecline));
+		toolBar.getItems().add(compile);
+		
+		imageFile = new File("C:/Users/Avinash/workspace_hci/hci/src/images/play-button.png");  
+		imageDecline = new Image(imageFile.toURI().toString(),20,20,true,true);
+		Button execute = new Button("Execute",new ImageView(imageDecline));
+		toolBar.getItems().add(execute);
+		
+		imageFile = new File("C:/Users/Avinash/workspace_hci/hci/src/images/debug.png");  
+		imageDecline = new Image(imageFile.toURI().toString(),20,20,true,true);
+		Button debug = new Button("Debug",new ImageView(imageDecline));
+		toolBar.getItems().add(debug);
+		
+		imageFile = new File("C:/Users/Avinash/workspace_hci/hci/src/images/coding.png");  
+		imageDecline = new Image(imageFile.toURI().toString(),20,20,true,true);
+		Button codeGenerate = new Button("Code Generate",new ImageView(imageDecline));
+		toolBar.getItems().add(codeGenerate);
+		
+		imageFile = new File("C:/Users/Avinash/workspace_hci/hci/src/images/settings.png");  
+		imageDecline = new Image(imageFile.toURI().toString(),20,20,true,true);
+		Button optimize = new Button("Optimize",new ImageView(imageDecline));
+		toolBar.getItems().add(optimize);
+		
+		imageFile = new File("C:/Users/Avinash/workspace_hci/hci/src/images/cloud-service.png");  
+		imageDecline = new Image(imageFile.toURI().toString(),20,20,true,true);
+		Button developerOption = new Button("Developer Option",new ImageView(imageDecline));
+		toolBar.getItems().add(developerOption);
+		
+		imageFile = new File("C:/Users/Avinash/workspace_hci/hci/src/images/sort.png");  
+		imageDecline = new Image(imageFile.toURI().toString(),20,20,true,true);
+		Button switchUser = new Button("Switch User",new ImageView(imageDecline));
+		toolBar.getItems().add(switchUser);
+		
+		imageFile = new File("C:/Users/Avinash/workspace_hci/hci/src/images/link.png");  
+		imageDecline = new Image(imageFile.toURI().toString(),20,20,true,true);
+		Button linking = new Button("Linking",new ImageView(imageDecline));
+		toolBar.getItems().add(linking);
+		
+		ComboBox allOptions = new ComboBox();
+		allOptions.setPromptText("All Options");
+		allOptions.getItems().add("Option 1");
+		allOptions.getItems().add("Option 2");
+		allOptions.getItems().add("Option 3");
+		toolBar.getItems().add(allOptions);
+		
+
+		revertEdits.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override 
+			public void handle(ActionEvent actionEvent) {
+				editor.revertEdits();
+			}
+		});
+
+		 VBox layout = new VBox(); 
+		 layout.setSpacing(10); 
+		 ObservableList list = layout.getChildren(); 
+		 list.addAll(title, toolBar, editor,revertEdits,labeledCode);  
+		 layout.setStyle("-fx-background-color: cornsilk; -fx-padding: 10;");
+		 
+	/*	// layout the scene.
+		final VBox layout = VBoxBuilder.create().spacing(10).children(
+				title,
+				HBoxBuilder.create().children(toolBar).build(),
+				editor,
+				HBoxBuilder.create().spacing(10).children(revertEdits).build(),
+				labeledCode
+				).build();
+		layout.setStyle("-fx-background-color: cornsilk; -fx-padding: 10;");*/
+
+
+		// display the scene.
+		final Scene scene = new Scene(layout);
+		stage.setScene(scene);
+		stage.setHeight(600);
+		stage.setWidth(970);
+		stage.show();
+	}
 }
