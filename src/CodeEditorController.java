96ae13f9b26d968921b96442d2c6ef05e92ffb38
Avinash Damodaran <avinash.damodaran@pivotree.com>
2020-04-09 23:12:44 -0400

    Developer Options

--- src/CodeEditorController.java
+++ src/CodeEditorController.java
@@ -14,11 +14,15 @@ import java.util.List;
 import org.controlsfx.control.CheckComboBox;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
@@ -60,24 +64,24 @@ public class CodeEditorController extends Application {
 		// create the editing controls.
 
 
-        MenuBar menuBar = new MenuBar();
+		MenuBar menuBar = new MenuBar();
 
-        // --- Menu File
-        Menu menuFile = new Menu("File");
-        MenuItem open = new MenuItem("Open");
-        menuFile.getItems().addAll(open);
+		// --- Menu File
+		Menu menuFile = new Menu("File");
+		MenuItem open = new MenuItem("Open");
+		menuFile.getItems().addAll(open);
 
 
-        // --- Menu Edit
-        Menu menuEdit = new Menu("Edit");
-        MenuItem copy = new MenuItem("Copy");
-        MenuItem cut = new MenuItem("Cut");
-        MenuItem paste = new MenuItem("Paste");
-        menuEdit.getItems().addAll(copy, cut, paste);
+		// --- Menu Edit
+		Menu menuEdit = new Menu("Edit");
+		MenuItem copy = new MenuItem("Copy");
+		MenuItem cut = new MenuItem("Cut");
+		MenuItem paste = new MenuItem("Paste");
+		menuEdit.getItems().addAll(copy, cut, paste);
 
-        // --- Menu View
+		// --- Menu View
 
-        menuBar.getMenus().addAll(menuFile, menuEdit);
+		menuBar.getMenus().addAll(menuFile, menuEdit);
 
 		Label title = new Label("CodeEditor");
 		title.setStyle("-fx-font-size: 20;");
@@ -91,7 +95,7 @@ public class CodeEditorController extends Application {
 		output.setPrefHeight(200); 
 		output.setPrefWidth(300);
 		final Button revertEdits = new Button("Erase All");
-		
+
 		String content = new String ( Files.readAllBytes( Paths.get("test.cpp") ) );
 		if(!content.isEmpty()) {
 			editor.setText(content);
@@ -103,46 +107,58 @@ public class CodeEditorController extends Application {
 		toolBar.setLayoutY(700);
 
 		Button compile = new Button("Compile");
-//		Image hammer = new Image(getClass().getResourceAsStream("images/hammer.png"),20,20,true,true);
-//		compile.setGraphic(new ImageView(hammer));
+		Image hammer = new Image(getClass().getResourceAsStream("images/hammer.png"),20,20,true,true);
+		compile.setGraphic(new ImageView(hammer));
 		toolBar.getItems().add(compile);
 
 		Button execute = new Button("Execute");
-//		Image play = new Image(getClass().getResourceAsStream("images/play-button.png"),20,20,true,true);
-//		execute.setGraphic(new ImageView(play));
+		Image play = new Image(getClass().getResourceAsStream("images/play-button.png"),20,20,true,true);
+		execute.setGraphic(new ImageView(play));
 		toolBar.getItems().add(execute);
 
 		Button debug = new Button("Debug");
-//		Image debugImg = new Image(getClass().getResourceAsStream("images/debug.png"),20,20,true,true);
-//		debug.setGraphic(new ImageView(debugImg));
+		Image debugImg = new Image(getClass().getResourceAsStream("images/debug.png"),20,20,true,true);
+		debug.setGraphic(new ImageView(debugImg));
 		toolBar.getItems().add(debug);
 
 		Button switchUser = new Button("Switch User");
-//		Image sort = new Image(getClass().getResourceAsStream("images/sort.png"),20,20,true,true);
-//		switchUser.setGraphic(new ImageView(sort));
+		Image sort = new Image(getClass().getResourceAsStream("images/sort.png"),20,20,true,true);
+		switchUser.setGraphic(new ImageView(sort));
 		toolBar.getItems().add(switchUser);
 
 		Button importFile = new Button("Import");
 		toolBar.getItems().add(importFile);	
-		
+
 		Button linking = new Button("Linking");
-//		Image linkImg = new Image(getClass().getResourceAsStream("images/link.png"),20,20,true,true);
-//		linking.setGraphic(new ImageView(linkImg));
+		Image linkImg = new Image(getClass().getResourceAsStream("images/link.png"),20,20,true,true);
+		linking.setGraphic(new ImageView(linkImg));
 		toolBar.getItems().add(linking);
 
 		Button codeGenerate = new Button("Code Generate");
-//		Image coding = new Image(getClass().getResourceAsStream("images/coding.png"),20,20,true,true);
-//		codeGenerate.setGraphic(new ImageView(coding));
+		Image coding = new Image(getClass().getResourceAsStream("images/coding.png"),20,20,true,true);
+		codeGenerate.setGraphic(new ImageView(coding));
 		toolBar.getItems().add(codeGenerate);
 
 		Button optimize = new Button("Optimize");
-//		Image setting = new Image(getClass().getResourceAsStream("images/settings.png"),20,20,true,true);
-//		optimize.setGraphic(new ImageView(setting));
+		Image setting = new Image(getClass().getResourceAsStream("images/settings.png"),20,20,true,true);
+		optimize.setGraphic(new ImageView(setting));
 		toolBar.getItems().add(optimize);
 
-		Button developerOption = new Button("Developer Option");
-//		Image cloudImg = new Image(getClass().getResourceAsStream("images/cloud-service.png"),20,20,true,true);
-//		developerOption.setGraphic(new ImageView(cloudImg));
+		String opt[] = {"Assembly File", "Executable File", "Binary File"};
+		ChoiceBox<String> developerOption = new ChoiceBox<>(FXCollections.observableArrayList(opt));
+		Platform.runLater(() -> {
+			SkinBase<ChoiceBox<String>> skin = (SkinBase<ChoiceBox<String>>) developerOption.getSkin();
+			// children contain only "Label label" and "StackPane openButton"
+			for (Node child : skin.getChildren()) {
+				if (child instanceof Label) {
+					Label label = (Label) child;
+					if (label.getText().isEmpty()) {
+						label.setText("Developer Options");
+					}
+					return;
+				}
+			}
+		});
 		toolBar.getItems().add(developerOption);
 
 		final ObservableList<String> options = FXCollections.observableArrayList();
@@ -153,12 +169,12 @@ public class CodeEditorController extends Application {
 			developerOption.setVisible(false);
 			//toolBar.getItems().removeAll(codeGenerate,developerOption,optimize);
 
-			options.addAll(codeGenerate.getText(),optimize.getText(),developerOption.getText());
+			options.addAll(codeGenerate.getText(),optimize.getText(),"Developer Option");
 		}
 		else if(user == Constants.userType.TYPICAL) {
 			developerOption.setVisible(false);
 			//toolBar.getItems().remove(developerOption);
-			options.addAll(developerOption.getText());
+			options.addAll("Developer Option");
 		}
 
 		// Create the CheckComboBox with the data 
@@ -227,8 +243,8 @@ public class CodeEditorController extends Application {
 		VBox layout = new VBox(); 
 		layout.setSpacing(10);
 		ObservableList list = layout.getChildren();
-        layout.getChildren().addAll(menuBar);
-        list.addAll(title, toolBar, editor,revertEdits,outputTitle,output);
+		layout.getChildren().addAll(menuBar);
+		list.addAll(title, toolBar, editor,revertEdits,outputTitle,output);
 		layout.setStyle("-fx-background-color: cornsilk; -fx-padding: 10;");
 
 		compile.setOnAction(new EventHandler<ActionEvent>() {
@@ -239,7 +255,7 @@ public class CodeEditorController extends Application {
 				if (editor.getText().length() == 0){
 					return;
 				}
-				
+
 				try {
 					FileWriter fileWriter = new FileWriter("test.cpp");
 					fileWriter.write(editor.getText());
@@ -267,20 +283,20 @@ public class CodeEditorController extends Application {
 			Main main= new Main();
 			@Override
 			public void handle(ActionEvent event) {
-	
+
 				try {
 					FileWriter fileWriter = new FileWriter("test.cpp");
 					fileWriter.write(editor.getText());
 					fileWriter.close();
 					main.start(stage);
-				
+
 				} catch (Exception e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
 		});
-		
+
 		importFile.setOnAction(new EventHandler<ActionEvent>() {
 
 			@Override
@@ -301,24 +317,67 @@ public class CodeEditorController extends Application {
 			}
 		});
 
-        open.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                FileChooser fileChooser = new FileChooser();
-                FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("CPP files (*.cpp)", "*.cpp");
-                fileChooser.getExtensionFilters().add(extFilter);
-                File file = fileChooser.showOpenDialog(stage);
-                String path=file.toString();
-                try {
-                    String content = new String ( Files.readAllBytes( Paths.get(path) ) );
-                    editor.setText(content);
-                } catch (IOException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-            }
-        });
+		open.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				FileChooser fileChooser = new FileChooser();
+				FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("CPP files (*.cpp)", "*.cpp");
+				fileChooser.getExtensionFilters().add(extFilter);
+				File file = fileChooser.showOpenDialog(stage);
+				String path=file.toString();
+				try {
+					String content = new String ( Files.readAllBytes( Paths.get(path) ) );
+					editor.setText(content);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
 
+
+		// add a listener 
+		developerOption.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
+
+			@Override
+			public void changed(ObservableValue ov, Number value, Number new_value) {
+				// TODO Auto-generated method stub
+				Runtime rt = Runtime.getRuntime();
+				if(opt[new_value.intValue()].equals("Assembly File")){
+					
+					try {
+						Process pr = rt.exec("g++ -S test.cpp");
+						output.setText("Assembly File Generated");
+						File file = new File("test.s");
+						java.awt.Desktop.getDesktop().open(file);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+
+				}
+				else if(opt[new_value.intValue()].equals("Executable File")){
+					try {
+						Process pr = rt.exec("g++ test.cpp -o test");
+						output.setText("Executable File Generated");
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+				}
+				else if(opt[new_value.intValue()].equals("Binary File")){
+					try {
+						Process pr = rt.exec("g++ test.cpp -o test.o");
+						output.setText("Binary File Generated ");
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+
+		});
 		// display the scene.
 		final Scene scene = new Scene(layout);
 		stage.setScene(scene);
